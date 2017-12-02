2d75d899e9c10bc73c9c14ed9de6b0af7bb74605
Tate Conlon <tate.conlon@gmail.com>
2017-12-02 18:23:40 -0500

    THIS IS ALL BULLLLLLLLLLLLLSHIT

--- backend/src/algorithm.py
+++ backend/src/algorithm.py
@@ -1,29 +1,128 @@
 import csv
 
+sizes = [4, 6, 8, 12] #must be sorted
+pizzas = []
 
 def algorithm():
 	data = parse("input.csv", ",")
+	legacy_data = list(data) #copy cuz we gonna fuk wit
 
 	fieldNames = data.pop(0)
 
 	d = {} #{toppings_id: num_slices}
 
-	for row in data:
-		_id = "cheese"
-		for i, topping in enumerate(data):
-			if i == 0: continue #first one is slices
-			if topping:
-				_id += "|{0}".format(fieldNames[i])
+	new_data = []
+	for x in data:
+		new_data.append(tuple(map(int, x)))
 
-		#add slices
-		if _id in d:
-			d[_id] += int(row[0])
-		else: 
-			d[_id] = int(row[0]) 
+	#print(toppings_matrix)
+	for i in new_data:
+		toppings = tuple(i[1:])
+		if tuple(toppings) in d.keys():
+			d[toppings] += i[0]
+		else:
+			d[toppings] = i[0]
+
+	while len(d) > 0:
+		a = find_most_unique(d)
+
+		if d[a] in sorted(sizes):
+			pizzas.append((a, d[a]))
+			d.pop(a)
+			print("perf size")
+		else:
+			for s in sorted(sizes):
+				if a in d.keys() and d[a] < s:
+					if len(d) == 1:
+						pizzas.append((a, s))
+						d.pop(a)
+						print("{0} | last pizza".format(a))
+						break
+					difference = s - d[a]
+					print("a:{za}|size:{0} | difference:{1}".format(s, difference, za=a))
+					while difference > 0:
+						temp = find_most_alike(d, a)
+						if temp == None:
+							pizzas.append((a, s))
+							print("{0} | difference {1}".format(a, difference))
+							break
+						elif d[temp] < difference:
+							difference -= d[temp]
+							d.pop(temp) #gone
+							print("popped {0} | difference {1}".format(temp, difference))
+						else:
+							d[temp] -= difference
+							difference = 0
+							pizzas.append((a, s))
+							print("made pizza {0} | difference {1}".format(temp, d[temp]))
+					d.pop(a)
+	print(pizzas)
 
 
+def find_most_unique(d):
+	'''Unique and lowest slices'''
+	toppings = list(d.keys())
+
+	result = {}
+
+	for x in toppings:
+		result[x] = 0
+
+	#most unique&picky
+	for i in range(0,len(toppings)):
+		for j in range(0, len(toppings)):
+				result[toppings[i]] += num_matches(toppings[i], toppings[j])
+
+	lowest_matches = min(result.values())
+
+	lowest_slices = (0, 99999999999999999999999999)
+
+	#lowest num of slices
+	for i in result.keys():
+		if result[i] == lowest_matches and d[i] < lowest_slices[1]:
+			lowest_slices = (i, d[i])
+
+	return lowest_slices[0]
+
+def find_most_alike(d, val):
+	'''most alike and most slices'''
+	toppings = list(d.keys())
+
+	toppings.remove(val)
+
+	result = {}
+
+	for x in toppings:
+		result[x] = 0
+
+	#most unique&picky
+	for t in toppings:
+		if val == t:
+			continue
+		for i in range(len(val)):
+				if val[i] == t[i]:
+					result[t] += 1
+
+	if len(result.values()) == 0:
+		return None
+	highest_matches = max(result.values())
+
+	lowest_slices = (0, 99999999999999999999999999)
+
+	#lowest num of slices
+	for i in result.keys():
+		if result[i] == highest_matches and d[i] < lowest_slices[1]:
+			lowest_slices = (i, d[i])
+
+	return lowest_slices[0]
 
 
+def num_matches(t1, t2):
+	count = 0
+	for i in range(len(t1)):
+		if t1[i] == 1 and t2[i] == 1:
+			count += 1
+	return count
 
 
 def parse(raw_file, delimiter):
